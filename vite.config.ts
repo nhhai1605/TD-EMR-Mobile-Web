2c7c96e89434ebd335dd4e13f0cf218386276ebe
nguyenhh <nhhai1605@gmail.com>
2023-06-06 00:04:12 +1000

    Remove eslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
-import eslintPlugin from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin(), eslintPlugin()],
+  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
   server: {
     port: 3000,
     open: true,
